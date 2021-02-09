6441e9ab8750b8cd91c7b66a64c8edf9f9bd22a5
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-09 15:34:26 +0900

    feat: add redux-logger

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import reducers from '../reducers';
+
+const middlewares = [logger];
 
 export default function configureStore() {
   return createStore(
     combineReducers({
       ...reducers
     }),
-    {}
+    applyMiddleware(...middlewares)
   );
 }
