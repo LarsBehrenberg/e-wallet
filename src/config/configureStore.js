fd93b68c1ed6bca3dbf6b4d8294837a5d265242c
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-12 20:27:07 +0900

    feat: implement redux thunk

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,8 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const middlewares = [logger];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export default function configureStore() {
   return createStore(
