8ff2cd9ccc15ccb6208ecf78abb8ab994fa448b7
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-13 16:25:49 +0900

    feat: display loader animation on firebase auth loading

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Redux
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import configureStore from './config/configureStore';
 
 // Firestore
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase';
 import firebase from './firebase/firebase.utils';
 import { createFirestoreInstance } from 'redux-firestore';
+
+import { RingLoader } from 'react-spinners';
 
 const { store, persistor } = configureStore();
 
@@ -23,11 +25,32 @@ const rrfProps = {
   createFirestoreInstance
 };
 
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth))
+    return (
+      <section className="vh-100">
+        <header className="container vh-100">
+          <div className="d-flex align-items-center justify-content-center vh-100">
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ width: '150px', height: '80px' }}>
+              <RingLoader color={'var(--primary)'} loading={true} />
+            </div>
+          </div>
+        </header>
+      </section>
+    );
+  return children;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={'loading'} persistor={persistor}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
+        <AuthIsLoaded>
+          <App />
+        </AuthIsLoaded>
       </ReactReduxFirebaseProvider>
     </PersistGate>
   </Provider>,
