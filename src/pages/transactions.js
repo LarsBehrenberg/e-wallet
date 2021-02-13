7d8a36e809ca2e5f68f3304c96cf3f7a2c5c17cf
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-13 12:17:01 +0900

    style: remove default transactions

--- src/pages/transactions.js
+++ src/pages/transactions.js
@@ -10,7 +10,7 @@ export default function Transactions() {
         <div className="card-header">
           <div className="card-header--title">
             <h4 className="font-size-lg mb-0 py-2 font-weight-bold">
-              Users list
+              Transaction list
             </h4>
           </div>
 
@@ -26,185 +26,49 @@ export default function Transactions() {
           <Table className="table table-borderless table-hover table-alternate text-nowrap mb-0">
             <thead>
               <tr>
-                <th>Software</th>
-                <th className="text-center">Income</th>
-                <th className="text-center">Expenses</th>
-                <th className="text-center">Status</th>
-                <th className="text-right">Totals</th>
+                <th></th>
+                <th>Date</th>
+                <th>Name</th>
+                <th className="text-center">Income/Expense</th>
+                <th className="text-center">Amount</th>
+                <th className="text-center">Wallet</th>
               </tr>
             </thead>
             <tbody>
               <tr>
+                <td className="">
+                  <Checkbox color="primary" id="checkboxProjects15" />
+                </td>
                 <td>
                   <div className="d-flex">
-                    <Checkbox
-                      color="primary"
-                      id="checkboxProjects15"
-                      className="align-self-center mr-2"
-                    />
                     <div>
-                      <a
-                        href="#/"
-                        onClick={(e) => e.preventDefault()}
-                        className="font-weight-bold text-black"
-                        title="...">
-                        Isaiah Ruiz
-                      </a>
-                      <span className="text-black-50 d-block">
-                        Senior Web Developer
+                      <span className="font-weight-bold text-black">
+                        {new Date().toJSON().slice(0, 10).replace(/-/g, '/')}
                       </span>
                     </div>
                   </div>
+                </td>
+
+                <td>
+                  <div className="d-flex">
+                    <div>
+                      <span className="font-weight-bold text-black">
+                        ETC fee
+                      </span>
+                      <span className="text-black-50 d-block">
+                        Transportation
+                      </span>
+                    </div>
+                  </div>
+                </td>
+                <td className="text-center">
+                  <span className="font-weight-bold ">Expense</span>
                 </td>
                 <td className="text-center">
                   <span className="font-weight-bold text-danger">-$254</span>
                 </td>
                 <td className="text-center">
-                  <span className="text-danger font-weight-bold">-2,374</span>
-                </td>
-                <td className="text-center">
-                  <div className="badge badge-danger">Overdue</div>
-                </td>
-                <td className="text-right">
-                  <div className="d-flex align-items-center justify-content-end">
-                    <div className="font-weight-bold font-size-lg pr-2">
-                      2363
-                    </div>
-                    <FontAwesomeIcon
-                      icon={['fas', 'arrow-down']}
-                      className="font-size-sm opacity-5"
-                    />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="d-flex">
-                    <Checkbox
-                      color="primary"
-                      id="checkboxProjects4"
-                      className="align-self-center mr-2"
-                    />
-                    <div>
-                      <a
-                        href="#/"
-                        onClick={(e) => e.preventDefault()}
-                        className="font-weight-bold text-black"
-                        title="...">
-                        Inez Conley
-                      </a>
-                      <span className="text-black-50 d-block">
-                        Project Manager
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td className="text-center">
-                  <span className="font-weight-bold">$18,386</span>
-                </td>
-                <td className="text-center">
-                  <span className="text-danger font-weight-bold">-6,310</span>
-                </td>
-                <td className="text-center">
-                  <div className="badge badge-warning">Pending</div>
-                </td>
-                <td className="text-right">
-                  <div className="d-flex align-items-center justify-content-end">
-                    <div className="font-weight-bold font-size-lg pr-2">
-                      584
-                    </div>
-                    <FontAwesomeIcon
-                      icon={['fas', 'arrow-down']}
-                      className="font-size-sm opacity-5"
-                    />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="d-flex">
-                    <Checkbox
-                      color="primary"
-                      id="checkboxProjects5"
-                      className="align-self-center mr-2"
-                    />
-                    <div>
-                      <a
-                        href="#/"
-                        onClick={(e) => e.preventDefault()}
-                        className="font-weight-bold text-black"
-                        title="...">
-                        Adyan Sosa
-                      </a>
-                      <span className="text-black-50 d-block">
-                        User Experience Designer
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td className="text-center">
-                  <span className="font-weight-bold">$6,356</span>
-                </td>
-                <td className="text-center">
-                  <span className="text-warning">-374</span>
-                </td>
-                <td className="text-center">
-                  <div className="badge badge-first">Waiting</div>
-                </td>
-                <td className="text-right">
-                  <div className="d-flex align-items-center justify-content-end">
-                    <div className="font-weight-bold font-size-lg pr-2">
-                      483
-                    </div>
-                    <FontAwesomeIcon
-                      icon={['fas', 'arrow-up']}
-                      className="font-size-sm opacity-5"
-                    />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="d-flex">
-                    <Checkbox
-                      color="primary"
-                      id="checkboxProjects11"
-                      className="align-self-center mr-2"
-                    />
-                    <div>
-                      <a
-                        href="#/"
-                        onClick={(e) => e.preventDefault()}
-                        className="font-weight-bold text-black"
-                        title="...">
-                        Beck Simpson
-                      </a>
-                      <span className="text-black-50 d-block">
-                        Senior Consultant
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td className="text-center">
-                  <span className="font-weight-bold">$16,281</span>
-                </td>
-                <td className="text-center">
-                  <span className="text-success">+684</span>
-                </td>
-                <td className="text-center">
-                  <div className="badge badge-success">Done</div>
-                </td>
-                <td className="text-right">
-                  <div className="d-flex align-items-center justify-content-end">
-                    <div className="font-weight-bold font-size-lg pr-2">
-                      {' '}
-                      $12,23M
-                    </div>
-                    <FontAwesomeIcon
-                      icon={['fas', 'arrow-up']}
-                      className="font-size-sm opacity-5"
-                    />
-                  </div>
+                  <span className="font-weight-bold">Visa</span>
                 </td>
               </tr>
             </tbody>
@@ -212,7 +76,7 @@ export default function Transactions() {
           <div className="divider mb-3" />
           <div className="text-center">
             <Button variant="contained" color="primary">
-              <span className="btn-wrapper--label">View details</span>
+              <span className="btn-wrapper--label">View more</span>
               <span className="btn-wrapper--icon">
                 <FontAwesomeIcon icon={['fas', 'chevron-right']} />
               </span>
