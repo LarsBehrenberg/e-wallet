be941ede524bc2b64f7217c0f95a5c492ae13b97
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-15 15:45:14 +0900

    feat: add custom categories to state

--- src/components/settingBlocks.js
+++ src/components/settingBlocks.js
@@ -1,17 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+// Redux & Firestore
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, useFirestore } from 'react-redux-firebase';
+
+// Components
+import { Grid, Card, Dialog, TextField, Button } from '@material-ui/core';
+
+// Assets & Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Grid, Card } from '@material-ui/core';
-
 import illustration1 from '../assets/images/illustrations/pack1/analysis.svg';
 import illustration2 from '../assets/images/illustrations/pack1/businessman.svg';
 
 export default function LivePreviewExample() {
+  // Access firestore data and set state
+  const firestore = useFirestore();
+  const { uid } = useSelector((state) => state.firebase.auth);
+  const { user } = useSelector((state) => state.firestore.data);
+
+  useFirestoreConnect([
+    {
+      collection: 'users',
+      doc: uid,
+      storeAs: 'user'
+    }
+  ]);
+
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userExpenseCategories, setUserExpenseCategories] = useState([]);
+  const [userIncomeCategories, setUserIncomeCategories] = useState([]);
+  const [newExpenseCategory, setNewExpenseCategory] = useState('');
+  const [newIncomeCategory, setNewIncomeCategory] = useState('');
+
+  // Modal toggle state
+  const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false);
+
+  const toggle1 = () => {
+    setModal1(!modal1);
+    setUserEmail(user.email);
+    setUserName(user.displayName);
+  };
+
+  const handleChange = (e, setState) => {
+    e.preventDefault();
+    setState(e.target.value);
+  };
+
+  const handleSubmitToFireStore = () => {
+    firestore.update(`users/${uid}/`, {
+      email: userEmail,
+      displayName: userName
+    });
+
+    setModal1(!modal1);
+  };
+
+  const toggle2 = () => {
+    setModal2(!modal2);
+    setUserExpenseCategories(user.expenseCategories);
+    setUserIncomeCategories(user.incomeCategories);
+  };
+
+  const removeCategory = (category, state, setState) => {
+    setState(state.filter((word) => word !== category));
+  };
+
   return (
     <>
       <Grid container spacing={6}>
         <Grid item md={6} lg={12} xl={6}>
-          <Card>
+          <Card onClick={() => toggle1()} style={{ cursor: 'pointer' }}>
             <div className="p-4">
               <Grid container spacing={0}>
                 <Grid item md={3}>
@@ -25,7 +85,7 @@ export default function LivePreviewExample() {
                 <Grid item md={9} className="d-flex align-items-center">
                   <div>
                     <div className="font-size-lg font-weight-bold mb-1">
-                      Informations
+                      Profile Information
                     </div>
                     <p className="opacity-7 font-size-md mb-0">
                       Acccess this page in order to manage and customize all
@@ -36,17 +96,14 @@ export default function LivePreviewExample() {
               </Grid>
             </div>
             <div className="divider" />
-            <a
-              href="#/"
-              onClick={(e) => e.preventDefault()}
-              className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
+            <span className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
               <div>Manage account</div>
               <FontAwesomeIcon icon={['fas', 'chevron-right']} />
-            </a>
+            </span>
           </Card>
         </Grid>
         <Grid item md={6} lg={12} xl={6}>
-          <Card>
+          <Card onClick={() => toggle2()} style={{ cursor: 'pointer' }}>
             <div className="p-4">
               <Grid container spacing={0}>
                 <Grid item md={3}>
@@ -81,6 +138,223 @@ export default function LivePreviewExample() {
           </Card>
         </Grid>
       </Grid>
+      <Dialog
+        scroll="body"
+        classes={{ paper: 'bg-transparent' }}
+        maxWidth="md"
+        open={modal1}
+        onClose={toggle1}>
+        <Grid container spacing={0}>
+          <div className="bg-white rounded">
+            <div className="p-4 text-center">
+              <h4 className="font-size-lg font-weight-bold my-2">
+                Profile Information
+              </h4>
+
+              <p className="text-muted mb-4 mx-4">
+                Access your profile information and change them if necessary.
+              </p>
+
+              <div className="divider my-4" />
+
+              <Grid container spacing={4}>
+                <Grid item xs={12} lg={12}>
+                  <div className="p-3">
+                    <TextField
+                      fullWidth
+                      className="m-2 mb-3"
+                      variant="outlined"
+                      id="textfield-name"
+                      label="Name"
+                      onChange={(e) => handleChange(e, setUserName)}
+                      defaultValue={userName}
+                    />
+                    <TextField
+                      fullWidth
+                      className="m-2"
+                      variant="outlined"
+                      id="textfield-email"
+                      label="Email"
+                      onChange={(e) => handleChange(e, setUserEmail)}
+                      defaultValue={userEmail}
+                    />
+                  </div>
+                </Grid>
+              </Grid>
+
+              <div className="divider my-4" />
+              <Button
+                onClick={() => handleSubmitToFireStore()}
+                className="btn btn-primary font-weight-bold text-uppercase text-black-70 text-center mb-3">
+                Save Changes
+              </Button>
+            </div>
+          </div>
+        </Grid>
+      </Dialog>
+      <Dialog
+        scroll="body"
+        classes={{ paper: 'bg-transparent' }}
+        maxWidth="md"
+        open={modal2}
+        onClose={toggle2}>
+        <Grid container spacing={0}>
+          <div className="bg-white rounded">
+            <div className="p-4 text-center">
+              <h4 className="font-size-lg font-weight-bold my-2">
+                Account Settings
+              </h4>
+              {/* <div className="text-center my-4">
+                <div className="badge badge-pill badge-neutral-first text-first mx-1">
+                  Web developer
+                </div>
+                <div className="badge badge-pill badge-neutral-warning text-warning mx-1">
+                  Javascript
+                </div>
+                <div className="badge badge-pill badge-neutral-danger text-danger mx-1">
+                  Angular
+                </div>
+              </div> */}
+
+              <p className="text-muted mb-4 mx-4">
+                Set your income and expense currencies as well as the currency
+                you want to store your transactions in.
+              </p>
+
+              <div className="divider my-4" />
+              <Grid container spacing={4} className="my-4">
+                {/* insert here */}
+                <Grid xs={12} md={6}>
+                  <p className="font-weight-bold mb-1">Income categories</p>
+                  <p className="text-muted mb-2 font-size-sm">
+                    Click a category to remove it from the list.
+                  </p>
+                  {userIncomeCategories.map((category) => (
+                    <div
+                      key={category}
+                      onClick={() =>
+                        removeCategory(
+                          category,
+                          userIncomeCategories,
+                          setUserIncomeCategories
+                        )
+                      }
+                      className="badge badge-pill badge-neutral-first text-first mx-1"
+                      style={{ cursor: 'pointer' }}>
+                      {category}
+                    </div>
+                  ))}
+                  <div className="d-flex align-content-center mt-3 justify-content-center">
+                    <TextField
+                      style={{
+                        maxWidth: '170px',
+                        marginRight: '1rem'
+                      }}
+                      onChange={(e) => {
+                        e.preventDefault();
+                        setNewIncomeCategory(e.target.value);
+                      }}
+                      variant="outlined"
+                      size="small"
+                      id="textfield-income-category"
+                      label="New Category"
+                      type="text"
+                    />
+                    <Button
+                      className="btn btn-neutral-first p-2"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if (
+                          newIncomeCategory.length !== 0 &&
+                          userIncomeCategories.find(
+                            (element) => element === newIncomeCategory
+                          ) === undefined
+                        ) {
+                          setUserIncomeCategories([
+                            ...userIncomeCategories,
+                            newIncomeCategory
+                          ]);
+                        }
+                      }}>
+                      Add
+                    </Button>
+                  </div>
+                </Grid>
+                <Grid xs={12} md={6} className="mt-4 mt-md-0">
+                  <p className="font-weight-bold mb-1">Expense categories</p>
+                  <p className="text-muted mb-2 font-size-sm">
+                    Click a category to remove it from the list.
+                  </p>
+
+                  {userExpenseCategories.map((category) => (
+                    <div
+                      key={category}
+                      onClick={() =>
+                        removeCategory(
+                          category,
+                          userExpenseCategories,
+                          setUserExpenseCategories
+                        )
+                      }
+                      className="badge badge-pill badge-neutral-first text-first mx-1"
+                      style={{ cursor: 'pointer' }}>
+                      {category}
+                    </div>
+                  ))}
+                  <div className="d-flex align-content-center mt-3 justify-content-center">
+                    <TextField
+                      style={{
+                        maxWidth: '170px',
+                        marginRight: '1rem'
+                      }}
+                      onChange={(e) => {
+                        e.preventDefault();
+                        setNewExpenseCategory(e.target.value);
+                      }}
+                      variant="outlined"
+                      size="small"
+                      id="textfield-expense-category"
+                      label="New Category"
+                      type="text"
+                    />
+                    <Button
+                      className="btn btn-neutral-first p-2"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if (
+                          newExpenseCategory.length !== 0 &&
+                          userExpenseCategories.find(
+                            (element) => element == newExpenseCategory
+                          ) === undefined
+                        ) {
+                          setUserExpenseCategories([
+                            ...userExpenseCategories,
+                            newExpenseCategory
+                          ]);
+                        }
+                      }}>
+                      Add
+                    </Button>
+                  </div>
+                </Grid>
+                <Grid xs={12} md={6} className="mx-auto mt-4">
+                  <p className="font-weight-bold mb-1">Expense categories</p>
+                  <p className="text-muted mb-2 font-size-sm">
+                    Click a category to remove it from the list.
+                  </p>
+                </Grid>
+              </Grid>
+
+              <div className="divider my-4" />
+              <Button
+                onClick={() => handleSubmitToFireStore()}
+                className="btn btn-primary font-weight-bold text-uppercase text-black-70 text-center mb-3">
+                Save Changes
+              </Button>
+            </div>
+          </div>
+        </Grid>
+      </Dialog>
     </>
   );
 }
