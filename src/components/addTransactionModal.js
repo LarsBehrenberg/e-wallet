3c51a191131b100e7371feaf86a9468b6476619b
Las Behrenberg <l.behrenberg@gmail.com>
2021-03-09 19:31:52 +0900

    feat: batch delete selected transactions

--- src/components/addTransactionModal.js
+++ src/components/addTransactionModal.js
@@ -115,6 +115,7 @@ const AddTransactionModal = () => {
           wallet,
           type,
           date: selectedDate,
+          createdAt: new Date(),
           category
         })
         .then((docRef) => {
--- src/pages/transactions.js
+++ src/pages/transactions.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Redux & Firebase
 import { useSelector } from 'react-redux';
 import { isLoaded, isEmpty } from 'react-redux-firebase';
+import { useFirestore } from 'react-redux-firebase';
 
 // Components
 import AddTransactionModal from '../components/addTransactionModal';
 import { Table, Checkbox, Card, CardContent, Button } from '@material-ui/core';
-
-// TRANSACTION REMOVE DUMMY FUNCTION
-// const remove = () => {
-//   firestore
-//     .collection('users')
-//     .doc(uid)
-//     .collection('transactions')
-//     .doc('bjAW4G045rDZ1cC1Xzr6')
-//     .delete();
-// };
 
 const Transaction = ({
   description,
@@ -25,50 +16,113 @@ const Transaction = ({
   amount,
   date,
   currency,
-  category
-}) => (
-  <tr>
-    <td>
-      <Checkbox color="primary" id="checkboxProjects15" />
-    </td>
-    <td>
-      <div className="d-flex">
-        <div>
-          <span className="font-weight-bold text-black">
-            {new Date(date.seconds * 1000).toISOString().split('T')[0]}
-          </span>
+  category,
+  isChecked,
+  transactionsID,
+  setChecked
+}) => {
+  return (
+    <tr>
+      <td>
+        <Checkbox
+          color="primary"
+          onChange={() => setChecked(transactionsID)}
+          checked={isChecked}
+        />
+      </td>
+      <td>
+        <div className="d-flex">
+          <div>
+            <span className="font-weight-bold text-black">
+              {new Date(date.seconds * 1000).toISOString().split('T')[0]}
+            </span>
+          </div>
         </div>
-      </div>
-    </td>
+      </td>
 
-    <td>
-      <div className="d-flex">
-        <div>
-          <span className="font-weight-bold text-black">{description}</span>
-          <span className="text-black-50 d-block">{category}</span>
+      <td>
+        <div className="d-flex">
+          <div>
+            <span className="font-weight-bold text-black">{description}</span>
+            <span className="text-black-50 d-block">{category}</span>
+          </div>
         </div>
-      </div>
-    </td>
-    <td className="text-center">
-      <span className="font-weight-bold ">{type}</span>
-    </td>
-    <td className="text-center">
-      <span
-        className={`font-weight-bold ${
-          type === 'Expense' ? 'text-danger' : 'text-success'
-        }`}>
-        {type === 'Expense' ? '-' : ''}
-        {`${amount} (${currency})`}
-      </span>
-    </td>
-    <td className="text-center">
-      <span className="font-weight-bold">{wallet}</span>
-    </td>
-  </tr>
-);
+      </td>
+      <td className="text-center">
+        <span className="font-weight-bold ">{type}</span>
+      </td>
+      <td className="text-center">
+        <span
+          className={`font-weight-bold ${
+            type === 'Expense' ? 'text-danger' : 'text-success'
+          }`}>
+          {type === 'Expense' ? '-' : ''}
+          {`${amount} (${currency})`}
+        </span>
+      </td>
+      <td className="text-center">
+        <span className="font-weight-bold">{wallet}</span>
+      </td>
+    </tr>
+  );
+};
 
 export default function Transactions() {
   const { transactions } = useSelector((state) => state.firestore.data);
+  const [state, setState] = useState([]);
+
+  const firestore = useFirestore();
+  const { uid } = useSelector((state) => state.firebase.auth);
+
+  useEffect(() => {
+    // Instead of using firestore transactions state we want to use local state for displaying all transactions. However, we need to wait with useEffect until transactions have loaded in to store them in the local state.
+    if (transactions) {
+      setState(
+        Object.keys(transactions).map((item) => {
+          return { ...transactions[item], isChecked: false };
+        })
+      );
+    }
+  }, [transactions]);
+
+  const setChecked = (id) => {
+    // Create shallow copy of selected item and reverse checked status
+    const currentItem = { ...state.find((item) => item.transactionsID === id) };
+    currentItem.isChecked = !currentItem.isChecked;
+
+    // Create copy of all transactions and replace old item with new checked/unchecked item
+    const updatedTransactions = [
+      ...state.filter((item) => item.transactionsID !== id),
+      currentItem
+    ];
+
+    // Update state with new checked/unchecked item
+    setState(updatedTransactions);
+  };
+
+  const deleteTransactions = () => {
+    // Get ids of all checked items
+    const transactionsToDelete = state
+      .filter((item) => item.isChecked === true)
+      .map((item) => item.transactionsID);
+
+    // Create batch commit
+    const batch = firestore.batch();
+
+    // Go through checked items and add to batch delete
+    transactionsToDelete.forEach((id) => {
+      const ref = firestore
+        .collection('users')
+        .doc(uid)
+        .collection('transactions')
+        .doc(id);
+
+      batch.delete(ref);
+    });
+
+    // Fire batch delete
+    batch.commit();
+  };
 
   return (
     <>
@@ -83,9 +137,14 @@ export default function Transactions() {
           <div className="card-header--actions">
             <div>
               <AddTransactionModal />
-              <Button size="small" className="btn-neutral-danger">
-                More Actions
-              </Button>
+              {!!state?.find((item) => item.isChecked === true) && (
+                <Button
+                  size="small"
+                  className="btn-neutral-danger"
+                  onClick={deleteTransactions}>
+                  Delete Items
+                </Button>
+              )}
             </div>
           </div>
         </div>
@@ -93,11 +152,13 @@ export default function Transactions() {
           {!isLoaded(transactions) && (
             <div className="text-center my-4">Loading...</div>
           )}
+
           {isLoaded(transactions) && isEmpty(transactions) && (
             <div className="text-center my-4">
               There are currently no transactions in your database.
             </div>
           )}
+
           {isLoaded(transactions) && !isEmpty(transactions) && (
             <Table className="table table-borderless table-hover table-alternate text-nowrap mb-0">
               <thead>
@@ -111,20 +172,23 @@ export default function Transactions() {
                 </tr>
               </thead>
               <tbody>
-                {Object.keys(transactions).map((key, id) => (
-                  <Transaction
-                    key={key}
-                    id={id}
-                    date={transactions[key].date}
-                    name={transactions[key].name}
-                    currency={transactions[key].currency}
-                    amount={transactions[key].amount}
-                    wallet={transactions[key].wallet}
-                    description={transactions[key].description}
-                    type={transactions[key].type}
-                    category={transactions[key].category}
-                  />
-                ))}
+                {state &&
+                  state
+                    .sort(function (a, b) {
+                      // Sort by
+                      return (
+                        new Date(b.date.seconds) - new Date(a.date.seconds) ||
+                        new Date(b.createdAt.seconds) -
+                          new Date(a.createdAt.seconds)
+                      );
+                    })
+                    .map(({ ...props }) => (
+                      <Transaction
+                        key={props.transactionsID}
+                        {...props}
+                        setChecked={setChecked}
+                      />
+                    ))}
               </tbody>
             </Table>
           )}
